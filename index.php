6ca8a9c2cd921eeaa35a91c179ff19630119c062
Codin <coding-web@hotmail.com>
2017-08-05 21:57:05 +0200

    Test commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php session_start();
-//Connection a la basse de données
+//Connection a la basse de données.
 include 'node_modules/PDO/connect.php';
 include 'node_modules/PDO/fonctions.php';
 $titre = 'connect';
